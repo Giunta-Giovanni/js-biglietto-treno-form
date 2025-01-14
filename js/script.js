3dd8a3ef81a76e44bcc69388ce843cd7582bcec0
GiuntaGiovanni <jhonniest741@gmail.com>
2025-01-14 01:16:28 +0100

    [feat] creazione dati di output con aggiunta funzione di messaggio di offerta e funzione di generazione numeri casuali per calcolo carrozza

--- js/script.js
+++ js/script.js
@@ -9,11 +9,19 @@ Scrivere un programma che chieda all’utente: - Il numero di chilometri da perc
 
 ************************/
 
-// salviamo i file di Input
+// salviamo i dati di Input
 const nameField = document.getElementById ('name-field');
 const kilometersField = document.getElementById ('kilometers-field');
 const ageField = document.getElementById ('age-field')
+const capField = document.getElementById ('cap-field')
 const form = document.getElementById ('ticket-form');
+
+// salviamo i dati di output
+const name = document.getElementById ('passenger-name');
+const offer = document.getElementById ('offer');
+const carriage = document.getElementById ('carriage')
+const cpCode = document.getElementById ('cp-code')
+const ticketPrice = document.getElementById ('ticket-price');
 
 // // tramite l'invio del form prendiamo i valori dei campi selezionati e li scriviamo nei blocchi
 form.addEventListener('submit', (event) => {
@@ -21,39 +29,27 @@ form.addEventListener('submit', (event) => {
     event.preventDefault();
 
     //salvo i valori di input e gli levo eventuali spazi con trim
-    const nameValue = nameField.value.trim(); //Nome e cognome
-    const kilometersValue = parseFloat(kilometersField.value.trim()); //kilometri
-    const ageValue = parseInt(ageField.value.trim()); //età
-    const price = calcTicketPrice (kilometersValue, ageValue); //calcolo del prezzo
+    const nameValue = nameField.value.trim();                           //Nome e cognome
+    const kilometersValue = parseFloat(kilometersField.value.trim());   //kilometri
+    const ageValue = ageField.value.trim();                             //età
+    const messageValue = offerMessage (ageValue);                       //messaggio
+    const carriageValue = randomNumber(8,1);                            //numero carrozza
+    const cpCodeValue = capField.value.trim();                          //CAP
+    const priceValue = calcTicketPrice (kilometersValue, ageValue);     //calcolo del prezzo
+
 
     // mostro il risultato
-    const risultato = `Name: ${nameValue}, Kilometers: ${kilometersValue}, Age: ${ageValue}, Prezzo del biglietto ${price}`;
-    console.log(risultato);
+    // const risultato = `Name: ${nameValue}, Kilometers: ${kilometersValue}, Fascia di età: ${ageValue}, Prezzo del biglietto ${price}`;
+    // console.log(risultato);
+
+    // collego gli input agli output
+    name.innerHTML = nameValue;
+    offer.innerHTML = messageValue;
+    carriage.innerHTML = carriageValue;
+    cpCode.innerHTML = cpCodeValue;
+    ticketPrice.innerHTML = priceValue;
+
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -67,23 +63,50 @@ function calcTicketPrice(chilometri, eta){
     // moltiplico i chilometri inseriti dall'utente con il prezzo al km e salvo la variabile
     let prezzotratta = chilometri * 0.21;
 
-    // mostro il prezzo intero della tratta
-    console.log("Il prezzo intero è di", prezzotratta.toFixed(2),"€");
-
-    // se l'eta del passeggero è minore di 18 applicare uno sconto del 20%
-    if (eta < 18) {
+    // se il passeggero è minorenne applicare uno sconto del 20%
+    if (eta === 'minorenne') {
 
         // sconto del 20%
         prezzotratta = prezzotratta * 0.80 
     
-    // altrimenti se l'eta del passeggero è maggiore di 65 applicare uno sconto del 40%   
-    } else if (eta > 65) {
+    // altrimenti se il passeggero è over 65 applicare uno sconto del 40%   
+    } else if (eta === 'over65') {
     
         // sconto del 40%
         prezzotratta = prezzotratta * 0.60
     }
 
 
-return `${prezzotratta.toFixed(2)} ${'€'}`
+    return `${prezzotratta.toFixed(2)} €`;
+
+}
+
+
+// funzione per calcolare il messaggio dell'offerta
+function offerMessage(eta){
+
+    let messaggio = 'Biglietto standard'
+    // se il passeggero è minorenne applicare uno sconto del 20%
+    if (eta === 'minorenne') {
+
+        // sconto del 20%
+        messaggio = 'Biglietto scontato del 20%'
+    
+    // se il passeggero è over 65 applicare uno sconto del 40%   
+    } else if (eta === 'over65') {
+    
+        // sconto del 40%
+        messaggio = 'Biglietto scontato del 40%'
+    }
+
+    return messaggio;
+
+}
+
+// funzione per calcolare numeri casuali
+function randomNumber(max,min){
+    const numeroGenerato = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return numeroGenerato;
 
 }
